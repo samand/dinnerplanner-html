4f7d33bb5fe1bfe4c1467df4710da941ef7eaf88
samand <samand@kth.se>
2018-01-28 22:07:05 +0100

    Dish details view in progress
    Need a revert point.

--- js/view/dishDetailsView.js
+++ js/view/dishDetailsView.js
@@ -17,10 +17,38 @@ var DishDetailsView =function(container,model){
 	this.dishImage[0].appendChild(this.img);
 
 	//Insert dish description
-	this.dishDescription = container.find("#DishDescription")
-	this.dishDescription.html(this.dish.description)
+	this.dishDescription = container.find("#DishDescription");
+	this.dishDescription.html(this.dish.description);
 
+/*
 	//TODO
 	//Create a list of ingredients to be displayed in a box. 
+	this.dishIngredients = container.find("#DishIngredients");
+	for (key in this.dish.ingredients){
+		//For each ingredient, create a div element
+		this.div = document.createElement('div');
+
+		//Create four more <div> elements,
+		this.ingr_name = document.createElement('div');
+		this.div.appendChild(this.ingr_name);
+		this.ingr_amount = document.createElement('div');
+		this.div.appendChild(this.ingr_amount);
+		this.ingr_unit = document.createElement('div');
+		this.div.appendChild(this.ingr_unit);
+		this.ingr_price = document.createElement('div');
+		this.div.appendChild(this.ingr_price);
+		/*this.ingr = this.dish.ingredients[key];
+		this.div.html = this.ingr.name;
+		console.log(this.ingr.name);
+		console.log(this.ingr.amount);
+		if (this.ingr.unit ==""){
+			console.log("there was no unit for this");
+		}
+		else {console.log(this.ingr.unit);}
+		console.log(this.ingr.price);
+		*/
+		this.dishIngredients[0].appendChild(this.div)
+	}
+	this.dishIngredients.html("This should contain the list of ingredients");
 
 }
