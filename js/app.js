c93987ab1f71d1bc956364c600f41c595026b55f
samand <samand@kth.se>
2018-02-01 16:35:24 +0100

    DinneerOverview
    TODO some formatting

--- js/app.js
+++ js/app.js
@@ -5,19 +5,11 @@ $(function() {
 	//Instantiating views
 
 	var sidebar = new SidebarView($("#sidebar"),dinnerModel);
+	var menuOverview = new MenuOverviewView($("#menuOverview"),dinnerModel);
 	var dishInstructions =new DishInstructionsView($("#dishInstructions"),dinnerModel);
 	var dishIngredients = new DishIngredientsView($("#dishIngredients"),dinnerModel);
 	var dishIcons = new DishIconView($("#dishIcons"),dinnerModel);
-	//var searchResults = new SearchResultsView(dinnerModel); //NOT RELEVANT
-
-
-
-
-
 	
-	// And create the instance of ExampleView
-	// var exampleView = new ExampleView($("#header"),dinnerModel);
-
 	/**
 	 * IMPORTANT: app.js is the only place where you are allowed to
 	 * use the $('someSelector') to search for elements in the whole HTML.
--- js/view/menuOverviewView.js
+++ js/view/menuOverviewView.js
@@ -1,20 +1,23 @@
-var DishIconView = function(container, model){
-	var dishesToBeDisplayed = [1,2,3];
-	for(key in dishesToBeDisplayed){
+var MenuOverviewView = function(container, model){
+	var menu = model.getMenu();
+	for(key in menu){
 		//Create placeholders and set formatting
 		var div = document.createElement("div");
 		div.className = "col-xs-6 col-md-4 border";
 		var img = document.createElement("img");
 		var dishName = document.createElement("div");
-
+		var dishPrice = document.createElement("div");
 		//Get dish and insert relevant things into the placeholders
-		var dish = model.getDish(dishesToBeDisplayed[key]);
+		var dish = model.getDish(menu[key]);
 		img.src = "images/".concat(dish.image);
 		dishName.innerHTML = dish.name;
+		var priceString = model.getDishPrice(menu[key]).toString().concat(" SEK");
+		dishPrice.innerHTML = priceString;
 
 		//Attach to parents
 		div.appendChild(img);
 		div.appendChild(dishName);
-		document.getElementById("dishIcons").appendChild(div);
+		div.appendChild(dishPrice)
+		container.append(div)
 	}
 }
