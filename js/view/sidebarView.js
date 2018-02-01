4ad5cfaf6979ae857143539456afb0796adfbc39
samand <samand@kth.se>
2018-02-01 10:08:03 +0100

    working revert point
    Working on sidebar

--- js/view/sidebarView.js
+++ js/view/sidebarView.js
@@ -1,4 +1,4 @@
-var MyDinnerSidebarView = function (model) {
+/*var MyDinnerSidebarView = function (model) {
 	//We've got a container, a div or similar, and a model with some functionality.
 	//We can now use the container to find some element inside it and change it using the functions in the model. 
 	if(document.getElementById("NumberOfGuests")){
@@ -12,7 +12,7 @@ var MyDinnerSidebarView = function (model) {
 		this.priceOfMenu.innerHTML= model.getTotalMenuPrice();
 	}
 }
-
+*/
 var VariableView = function(model){
 	if(document.getElementById("NumberOfGuests")){
 		this.numberOfGuests = document.getElementById("NumberOfGuests");
@@ -22,4 +22,6 @@ var VariableView = function(model){
 		this.priceOfMenu = document.getElementById("PriceOfMenu");
 		this.priceOfMenu.innerHTML= model.getTotalMenuPrice();
 	}
+	
+
 }
