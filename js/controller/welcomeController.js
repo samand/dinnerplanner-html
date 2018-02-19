09229bf0a45a70b5bca89db83a5ae884d311c3cd
Savvas <sappapad@hotmail.com>
2018-02-19 10:56:19 +0100

    welcome fix

--- js/controller/welcomeController.js
+++ js/controller/welcomeController.js
@@ -1,5 +1,6 @@
 var WelcomeController = function(model, view, app) {
 	//Create new dinner
-    var welcomeBtn= document.getElementById("welcomeButton");
+    //var welcomeBtn= document.getElementById("welcomeButton");
+    var welcomeBtn = view.editWelcome;
     welcomeBtn.addEventListener("click", app.showDishSearchScreen, false);
 }
