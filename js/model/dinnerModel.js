34c97a3e8c8a1de5f3114ba56c3acf26911d1e1d
samand <samand@kth.se>
2018-02-27 23:56:14 +0100

    First attempt at ajax() - lab3
    Clearly not working, only commiting for lab 3.

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -5,7 +5,10 @@ var DinnerModel = function() {
 	var searchResults=[]; //For dishSelectView
 	var currentDish; //For dishDetailsView
 	var observers =[]; //An array of update functions. The observers come from the views.
-	//Lab 2
+
+    var api_url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search";
+    var api_key = "Qu9grxVNWpmshA4Kl9pTwyiJxVGUp1lKzrZjsnghQMkFkfA4LB"
+	
 	this.addObserver = function(observer){
 		observers.push(observer);
 	}
@@ -135,6 +138,7 @@ var DinnerModel = function() {
 	//function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
 	//you can use the filter argument to filter out the dish by name or ingredient (use for search)
 	//if you don't pass any filter all the dishes will be returned
+    /*
 	this.getAllDishes = function (type,filter) {
 	  return dishes.filter(function(dish) {
 		var found = true;
@@ -153,10 +157,37 @@ var DinnerModel = function() {
 	  	return dish.type == type && found;
 	  });
 	}
+    */
+    var getAllDishesSuccess = function(data){
+        console.log(data.results);
+        searchResults = data.results;
+        notifyObservers("searchResults");
+    }
 
-	this.search = function(type,filter){
-		searchResults = this.getAllDishes(type,filter);
-		notifyObservers("searchResults"); //Questionable if searchResults should be handled like this in the model.
+    this.getAllDishes = function (type, filter){ //, callback, errorCallback
+        console.log(type);
+        $.ajax({
+            url: api_url,
+            headers: {
+                'X-Mashape-Key': api_key,
+                'type':type,
+                'query':filter
+            },
+            success: function(data){
+                getAllDishesSuccess(data)
+            },
+            error: function(error){
+                console.log(error);
+                //errorCallback(error)
+            }
+        })
+    }
+
+    this.search = function(type,filter){
+        searchResults = this.getAllDishes(type,filter);
+        console.log("searchResults")
+        console.log(searchResults)
+        notifyObservers("searchResults"); //Questionable if searchResults should be handled like this in the model.
 	}
 	this.getSearchResults = function(){
 		return searchResults;
@@ -164,12 +195,12 @@ var DinnerModel = function() {
 
 	//function that returns a dish of specific ID
 	this.getDish = function (id){
-	  	for(var key in dishes){
-			if(dishes[key].id == id) {
-				return dishes[key];
-			}
-		}
-	}
+        for(var key in dishes){
+         if(dishes[key].id == id) {
+            return dishes[key];
+        }
+    }
+}
 
 
 	// the dishes variable contains an array of all the
@@ -191,235 +222,235 @@ var DinnerModel = function() {
 			'quantity':0.5,
 			'unit':'',
 			'price':10
-			},{
-			'name':'milk',
-			'quantity':30,
-			'unit':'ml',
-			'price':6
-			},{
-			'name':'brown sugar',
-			'quantity':7,
-			'unit':'g',
-			'price':1
-			},{
-			'name':'ground nutmeg',
-			'quantity':0.5,
-			'unit':'g',
-			'price':12
-			},{
-			'name':'white bread',
-			'quantity':2,
-			'unit':'slices',
-			'price':2
-			}]
-		},{
-		'id':2,
-		'name':'Sourdough Starter',
-		'type':'starter',
-		'image':'sourdough.jpg',
-		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{
-			'name':'active dry yeast',
-			'quantity':0.5,
-			'unit':'g',
-			'price':4
-			},{
-			'name':'warm water',
-			'quantity':30,
-			'unit':'ml',
-			'price':0
-			},{
-			'name':'all-purpose flour',
-			'quantity':15,
-			'unit':'g',
-			'price':2
-			}]
-		},{
-		'id':3,
-		'name':'Baked Brie with Peaches',
-		'type':'starter',
-		'image':'bakedbrie.jpg',
-		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{
-			'name':'round Brie cheese',
-			'quantity':10,
-			'unit':'g',
-			'price':8
-			},{
-			'name':'raspberry preserves',
-			'quantity':15,
-			'unit':'g',
-			'price':10
-			},{
-			'name':'peaches',
-			'quantity':1,
-			'unit':'',
-			'price':4
-			}]
-		},{
-		'id':100,
-		'name':'Meat balls',
-		'type':'main dish',
-		'image':'meatballs.jpg',
-		'description':"Preheat an oven to 400 degrees F (200 degrees C). Place the beef into a mixing bowl, and season with salt, onion, garlic salt, Italian seasoning, oregano, red pepper flakes, hot pepper sauce, and Worcestershire sauce; mix well. Add the milk, Parmesan cheese, and bread crumbs. Mix until evenly blended, then form into 1 1/2-inch meatballs, and place onto a baking sheet. Bake in the preheated oven until no longer pink in the center, 20 to 25 minutes.",
-		'ingredients':[{
-			'name':'extra lean ground beef',
-			'quantity':115,
-			'unit':'g',
-			'price':20
-			},{
-			'name':'sea salt',
-			'quantity':0.7,
-			'unit':'g',
-			'price':3
-			},{
-			'name':'small onion, diced',
-			'quantity':0.25,
-			'unit':'',
-			'price':2
-			},{
-			'name':'garlic salt',
-			'quantity':0.7,
-			'unit':'g',
-			'price':2
-			},{
-			'name':'Italian seasoning',
-			'quantity':0.6,
-			'unit':'g',
-			'price':3
-			},{
-			'name':'dried oregano',
-			'quantity':0.3,
-			'unit':'g',
-			'price':3
-			},{
-			'name':'crushed red pepper flakes',
-			'quantity':0.6,
-			'unit':'g',
-			'price':3
-			},{
-			'name':'Worcestershire sauce',
-			'quantity':6,
-			'unit':'ml',
-			'price':7
-			},{
-			'name':'milk',
-			'quantity':20,
-			'unit':'ml',
-			'price':4
-			},{
-			'name':'grated Parmesan cheese',
-			'quantity':5,
-			'unit':'g',
-			'price':8
-			},{
-			'name':'seasoned bread crumbs',
-			'quantity':15,
-			'unit':'g',
-			'price':4
-			}]
-		},{
-		'id':101,
-		'name':'MD 2',
-		'type':'main dish',
-		'image':'bakedbrie.jpg',
-		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{
-			'name':'ingredient 1',
-			'quantity':1,
-			'unit':'pieces',
-			'price':8
-			},{
-			'name':'ingredient 2',
-			'quantity':15,
-			'unit':'g',
-			'price':7
-			},{
-			'name':'ingredient 3',
-			'quantity':10,
-			'unit':'ml',
-			'price':4
-			}]
-		},{
-		'id':102,
-		'name':'MD 3',
-		'type':'main dish',
-		'image':'meatballs.jpg',
-		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{
-			'name':'ingredient 1',
-			'quantity':2,
-			'unit':'pieces',
-			'price':8
-			},{
-			'name':'ingredient 2',
-			'quantity':10,
-			'unit':'g',
-			'price':7
-			},{
-			'name':'ingredient 3',
-			'quantity':5,
-			'unit':'ml',
-			'price':4
-			}]
-		},{
-		'id':103,
-		'name':'MD 4',
-		'type':'main dish',
-		'image':'meatballs.jpg',
-		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{
-			'name':'ingredient 1',
-			'quantity':1,
-			'unit':'pieces',
-			'price':4
-			},{
-			'name':'ingredient 2',
-			'quantity':12,
-			'unit':'g',
-			'price':7
-			},{
-			'name':'ingredient 3',
-			'quantity':6,
-			'unit':'ml',
-			'price':4
-			}]
-		},{
-		'id':200,
-		'name':'Chocolat Ice cream',
-		'type':'dessert',
-		'image':'icecream.jpg',
-		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{
-			'name':'ice cream',
-			'quantity':100,
-			'unit':'ml',
-			'price':6
-			}]
-		},{
-		'id':201,
-		'name':'Vanilla Ice cream',
-		'type':'dessert',
-		'image':'icecream.jpg',
-		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{
-			'name':'ice cream',
-			'quantity':100,
-			'unit':'ml',
-			'price':6
-			}]
-		},{
-		'id':202,
-		'name':'Strawberry',
-		'type':'dessert',
-		'image':'icecream.jpg',
-		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{
-			'name':'ice cream',
-			'quantity':100,
-			'unit':'ml',
-			'price':6
-			}]
-		}
-	];
+     },{
+         'name':'milk',
+         'quantity':30,
+         'unit':'ml',
+         'price':6
+     },{
+         'name':'brown sugar',
+         'quantity':7,
+         'unit':'g',
+         'price':1
+     },{
+         'name':'ground nutmeg',
+         'quantity':0.5,
+         'unit':'g',
+         'price':12
+     },{
+         'name':'white bread',
+         'quantity':2,
+         'unit':'slices',
+         'price':2
+     }]
+ },{
+  'id':2,
+  'name':'Sourdough Starter',
+  'type':'starter',
+  'image':'sourdough.jpg',
+  'description':"Here is how you make it... Lore ipsum...",
+  'ingredients':[{
+     'name':'active dry yeast',
+     'quantity':0.5,
+     'unit':'g',
+     'price':4
+ },{
+     'name':'warm water',
+     'quantity':30,
+     'unit':'ml',
+     'price':0
+ },{
+     'name':'all-purpose flour',
+     'quantity':15,
+     'unit':'g',
+     'price':2
+ }]
+},{
+  'id':3,
+  'name':'Baked Brie with Peaches',
+  'type':'starter',
+  'image':'bakedbrie.jpg',
+  'description':"Here is how you make it... Lore ipsum...",
+  'ingredients':[{
+     'name':'round Brie cheese',
+     'quantity':10,
+     'unit':'g',
+     'price':8
+ },{
+     'name':'raspberry preserves',
+     'quantity':15,
+     'unit':'g',
+     'price':10
+ },{
+     'name':'peaches',
+     'quantity':1,
+     'unit':'',
+     'price':4
+ }]
+},{
+  'id':100,
+  'name':'Meat balls',
+  'type':'main dish',
+  'image':'meatballs.jpg',
+  'description':"Preheat an oven to 400 degrees F (200 degrees C). Place the beef into a mixing bowl, and season with salt, onion, garlic salt, Italian seasoning, oregano, red pepper flakes, hot pepper sauce, and Worcestershire sauce; mix well. Add the milk, Parmesan cheese, and bread crumbs. Mix until evenly blended, then form into 1 1/2-inch meatballs, and place onto a baking sheet. Bake in the preheated oven until no longer pink in the center, 20 to 25 minutes.",
+  'ingredients':[{
+     'name':'extra lean ground beef',
+     'quantity':115,
+     'unit':'g',
+     'price':20
+ },{
+     'name':'sea salt',
+     'quantity':0.7,
+     'unit':'g',
+     'price':3
+ },{
+     'name':'small onion, diced',
+     'quantity':0.25,
+     'unit':'',
+     'price':2
+ },{
+     'name':'garlic salt',
+     'quantity':0.7,
+     'unit':'g',
+     'price':2
+ },{
+     'name':'Italian seasoning',
+     'quantity':0.6,
+     'unit':'g',
+     'price':3
+ },{
+     'name':'dried oregano',
+     'quantity':0.3,
+     'unit':'g',
+     'price':3
+ },{
+     'name':'crushed red pepper flakes',
+     'quantity':0.6,
+     'unit':'g',
+     'price':3
+ },{
+     'name':'Worcestershire sauce',
+     'quantity':6,
+     'unit':'ml',
+     'price':7
+ },{
+     'name':'milk',
+     'quantity':20,
+     'unit':'ml',
+     'price':4
+ },{
+     'name':'grated Parmesan cheese',
+     'quantity':5,
+     'unit':'g',
+     'price':8
+ },{
+     'name':'seasoned bread crumbs',
+     'quantity':15,
+     'unit':'g',
+     'price':4
+ }]
+},{
+  'id':101,
+  'name':'MD 2',
+  'type':'main dish',
+  'image':'bakedbrie.jpg',
+  'description':"Here is how you make it... Lore ipsum...",
+  'ingredients':[{
+     'name':'ingredient 1',
+     'quantity':1,
+     'unit':'pieces',
+     'price':8
+ },{
+     'name':'ingredient 2',
+     'quantity':15,
+     'unit':'g',
+     'price':7
+ },{
+     'name':'ingredient 3',
+     'quantity':10,
+     'unit':'ml',
+     'price':4
+ }]
+},{
+  'id':102,
+  'name':'MD 3',
+  'type':'main dish',
+  'image':'meatballs.jpg',
+  'description':"Here is how you make it... Lore ipsum...",
+  'ingredients':[{
+     'name':'ingredient 1',
+     'quantity':2,
+     'unit':'pieces',
+     'price':8
+ },{
+     'name':'ingredient 2',
+     'quantity':10,
+     'unit':'g',
+     'price':7
+ },{
+     'name':'ingredient 3',
+     'quantity':5,
+     'unit':'ml',
+     'price':4
+ }]
+},{
+  'id':103,
+  'name':'MD 4',
+  'type':'main dish',
+  'image':'meatballs.jpg',
+  'description':"Here is how you make it... Lore ipsum...",
+  'ingredients':[{
+     'name':'ingredient 1',
+     'quantity':1,
+     'unit':'pieces',
+     'price':4
+ },{
+     'name':'ingredient 2',
+     'quantity':12,
+     'unit':'g',
+     'price':7
+ },{
+     'name':'ingredient 3',
+     'quantity':6,
+     'unit':'ml',
+     'price':4
+ }]
+},{
+  'id':200,
+  'name':'Chocolat Ice cream',
+  'type':'dessert',
+  'image':'icecream.jpg',
+  'description':"Here is how you make it... Lore ipsum...",
+  'ingredients':[{
+     'name':'ice cream',
+     'quantity':100,
+     'unit':'ml',
+     'price':6
+ }]
+},{
+  'id':201,
+  'name':'Vanilla Ice cream',
+  'type':'dessert',
+  'image':'icecream.jpg',
+  'description':"Here is how you make it... Lore ipsum...",
+  'ingredients':[{
+     'name':'ice cream',
+     'quantity':100,
+     'unit':'ml',
+     'price':6
+ }]
+},{
+  'id':202,
+  'name':'Strawberry',
+  'type':'dessert',
+  'image':'icecream.jpg',
+  'description':"Here is how you make it... Lore ipsum...",
+  'ingredients':[{
+     'name':'ice cream',
+     'quantity':100,
+     'unit':'ml',
+     'price':6
+ }]
+}
+];
 }
